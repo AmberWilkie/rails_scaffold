e0351ee3146fb1a7a375d619674e1d7773e03bfa
Amber <amber@amberwilkie.com>
2016-09-05 14:56:14 +0200

    Iterate through @articles to print title and content on landing view

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,6 +2,6 @@ When(/^I am on the landing page$/) do
   visit root_path
 end
 
-Then(/^I should see "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^I should see "([^"]*)"$/) do |content|
+  expect(page).to have_content content
 end
