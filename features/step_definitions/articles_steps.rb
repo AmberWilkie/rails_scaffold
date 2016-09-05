2d7403461513e9f9db03499472cf59892afc00d3
Amber <amber@amberwilkie.com>
2016-09-05 15:00:33 +0200

    Add factory for test data instead of using Article table

--- features/step_definitions/articles_steps.rb
+++ features/step_definitions/articles_steps.rb
@@ -1,6 +1,7 @@
 Given(/^the following articles exist$/) do |table|
   # table is a Cucumber::MultilineArgument::DataTable
   table.hashes.each do |hash|
-    Article.create!(hash)
+    # Article.create!(hash)
+    FactoryGirl.create(:article, hash)
   end
 end
