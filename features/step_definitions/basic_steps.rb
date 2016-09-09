3c3f1f24cbb1fb6ac69abb2355a62d6951b282f2
Amber <amber@amberwilkie.com>
2016-09-09 17:10:13 +0200

    Create test for viewing the correct article

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -9,3 +9,20 @@ end
 When(/^I click the "([^"]*)" link$/) do |link|
   click_link(link)
 end
+
+Given(/^I am on the "([^"]*)" page$/) do |page|
+  article = Article.find_by(title: page)
+  visit article_path(article[:id])
+end
+
+Then(/^I should see the "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^click the "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be on the "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/comment_steps.rb
@@ -0,0 +1,3 @@
+When(/^I fill in the textbox with "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
