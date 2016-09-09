f927a00e93d7093623ebf1e44d566bdf4e8aa7ca
Amber <amber@amberwilkie.com>
2016-09-09 16:58:15 +0200

    Set up comment feature file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
-    # binding.pry
     render :article
   end
 end
--- features/step_definitions/articles_steps.rb
+++ features/step_definitions/articles_steps.rb
@@ -1,7 +1,5 @@
 Given(/^the following articles exist$/) do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
   table.hashes.each do |hash|
-    # Article.create!(hash)
     FactoryGirl.create(:article, hash)
   end
 end
