51d83c6c946eb9ebb081b319625c91d91da0b60e
Amber <amber@amberwilkie.com>
2016-09-09 17:15:44 +0200

    Set up Rspec test for Comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :body }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryGirl.create(:comment)).to be_valid
+    end
+  end
+end
