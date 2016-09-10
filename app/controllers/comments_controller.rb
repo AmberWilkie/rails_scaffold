c6a91a693332380d0e7604ac74060b2dca5b1c9b
Amber <amber@amberwilkie.com>
2016-09-10 21:55:45 +0200

    Remove acceptance test for non-existent article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,22 @@
 class CommentsController < ApplicationController
   def create
-    if params[:comment][:article_id] == nil
-      flash[:notice] = "Fuck, that article is gone, man"
-      redirect_to root_path
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new(comment_params)
+    if @comment.save
+      redirect_to article_path(@article)
     else
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.new(comment_params)
-      if @comment.save
-        redirect_to article_path(@article)
-      else
-        @article.reload
-        flash[:notice] = "You probably entered a crap email"
-        render 'articles/show'
-      end
+      email_problems
     end
-
   end
 
   private
     def comment_params
       params.require(:comment).permit(:body, :email)
+    end
+
+    def email_problems
+      @article.reload
+      flash[:notice] = "You probably entered a crap email"
+      render 'articles/show'
     end
 end
