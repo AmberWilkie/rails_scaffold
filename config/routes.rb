42efa6a0981d4eb663dcbdb78538cb9e031933bc
Amber <amber@amberwilkie.com>
2016-09-10 22:08:35 +0200

    Limit routes of articles and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles do
-    resources :comments
+  resources :articles, :only => [:show] do
+    resources :comments, :only => [:create]
   end
 
   root controller: :landing, action: :index
