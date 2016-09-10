a7364888ffed4f1fdea1c6ab13a1cb13af20a269
Amber <amber@amberwilkie.com>
2016-09-10 21:56:44 +0200

    Refactor comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,20 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.new(comment_params)
-    if @comment.save
-      redirect_to article_path(@article)
-    else
-      email_problems
-    end
+    try_saving_comment
   end
 
   private
     def comment_params
       params.require(:comment).permit(:body, :email)
+    end
+
+    def try_saving_comment
+      if @comment.save
+        redirect_to article_path(@article)
+      else
+        email_problems
+      end
     end
 
     def email_problems
