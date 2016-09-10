24e54cd6074926dbe099424e1768f7cfbfdd7d50
Amber <amber@amberwilkie.com>
2016-09-10 15:40:11 +0200

    WIP display article comments on article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
-    # @comment = Comment.create(article_id: @article.id)
     render :article
   end
 end
