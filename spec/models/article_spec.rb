d7f2a5a4f1bca8e33522a5a8e1281d951afe8047
Amber <amber@amberwilkie.com>
2016-09-11 08:27:30 +0200

    Refactor article spec with shoulda-matcher for dependents

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Article, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :content }
-    it { should have_many :comments}
+    it { should have_many(:comments).dependent(:destroy)}
   end
 
   describe 'Factory' do
@@ -25,11 +25,6 @@ RSpec.describe Article, type: :model do
       FactoryGirl.create(:article, title: "Title One")
       FactoryGirl.create(:article, title: "Title Two")
       FactoryGirl.create(:comment, article_id: Article.find_by(title: "Title One").id)
-    end
-    it 'should have no comments if article is destroyed' do
-      expect(Comment.all).not_to eq []
-      Article.destroy(Article.find_by(title: "Title One").id)
-      expect(Comment.all).to eq []
     end
   end
 end
